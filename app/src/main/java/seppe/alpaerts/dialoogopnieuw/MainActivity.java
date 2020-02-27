6ab0cdefd6feb320f249e24a9eb14ba019b38ce6
mobapp11 <mobapp11@student.ehb.be>
2020-02-27 11:25:03 +0100

    Initial commit

---
+++ app/src/main/java/seppe/alpaerts/dialoogopnieuw/MainActivity.java
@@ -0,0 +1,27 @@
+package seppe.alpaerts.dialoogopnieuw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_container);
+        AppBarConfiguration mAppBarConfiguration = new AppBarConfiguration.Builder().build();
+
+    }
+
+
+}
